9b230df215d021e40a2261c44170ad680ccc774a
Takahiro Yano <Yano_Takahiro@ogis-ri.co.jp>
2014-08-02 12:57:40 +0900

    Add CarList

---
+++ js/collections/cars.js
@@ -0,0 +1,21 @@
+var app = app || {};
+
+// Carコレクション
+
+var CarList = Backbone.Collection.extend({
+  model: app.Car,
+  localStorage: new Backbone.LocalStorage('cars-backbone'),
+
+  nextOrder: function () {
+    if (!this.length) {
+      return 1;
+    }
+    return this.last().get('order') + 1;
+  },
+
+  comparator: function (car) {
+    return car.get('order');
+  }
+});
+
+app.Cars = new CarList();
